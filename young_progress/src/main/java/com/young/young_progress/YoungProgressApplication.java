aec57455db3072ee08bc830d022281a7b377eecf
AndreaRakotondramanana <andrearakotondramanana0@gmail.com>
2024-07-12 14:33:43 +0300

    Andrea1
    test

---
+++ young_progress/src/main/java/com/young/young_progress/YoungProgressApplication.java
@@ -0,0 +1,13 @@
+package com.young.young_progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungProgressApplication.class, args);
+	}
+
+}
